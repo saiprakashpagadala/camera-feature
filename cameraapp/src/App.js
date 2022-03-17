c9416778351dafcf43d8053ba7ae6c90e4a42b73
sai prakash <saiprakashpagadala95@gmail.com>
2022-03-17 10:32:54 +0530

    camera feature

---
+++ cameraapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TestCamera from './Components/Camera/camera';
+
+function App() {
+  return (
+    <div className="App">
+      <TestCamera/>
+    </div>
+  );
+}
+
+export default App;
